4735e9edef831c1d958463e3ac3eb47d4f3dc6e2
tanhi141 <ta.nhi.633@gmail.com>
2018-08-05 15:58:01 +0700

    fix

--- FindHome/Main/All Post/AllPostPresenter.swift
+++ FindHome/Main/All Post/AllPostPresenter.swift
@@ -24,22 +24,22 @@ class AllPostPresenter: AllPostPresenting{
         
             
 //        let ref = Database.database().reference().child("Post")
-//        
+//
 //        ref.observeSingleEvent(of: .value, with: {[weak self] snapshot in
-//            
+//
 //            if !snapshot.exists() {
 //                return
 //            }
-//            
+//
 //            let user = snapshot.value as! [String: Any]
 //            let infoUser = user.values.first as? [String: String]
-            
+        
 //            User.share.email = infoUser?["email"]  ?? ""
 //            User.share.fullName = infoUser?["fullName"] ?? ""
 //            User.share.phonenNumber = infoUser?["phoneNumber"] ?? ""
 //            User.share.idUser = infoUser?["idUser"] ?? ""
             
-        })
+//        })
         
         
     }
